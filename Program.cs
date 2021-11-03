e8e30259fc9fd903d44c99843f13b0627006c87e
Egor4ik325 <zorinegor325@gmail.com>
2021-11-03 17:38:54 +0300

    Create project + add gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace telegram_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
