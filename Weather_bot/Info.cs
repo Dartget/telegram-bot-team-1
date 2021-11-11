126cae6172935014b905a511daa94035842c83cb
4erepashk <93653624+4erepashk@users.noreply.github.com>
2021-11-11 11:37:36 +0300

    Файлы которые нужно обработать

---
+++ Weather_bot/Info.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherBot
+{
+    public class TemperatureInfo
+    {
+        public float Temp { get; set; }
+        public float Feels_like {get;set;}
+
+    }
+
+    public class Window
+    {
+        public string Speed {get;set;}
+
+    }
+
+    
+
+
+
+    
+
+}
