0a9c7319fc92fd999336ab9df5377d0838bd21c6
Egor-N-S <63055406+Egor-S-N@users.noreply.github.com>
2021-11-16 17:59:12 +0300

    Add files via upload

---
+++ tg_bot_with_weather_search/telegram-bot/BotConfiguration.cs
@@ -0,0 +1,12 @@
+namespace TelegramBot
+{
+    public class BotConfiguration
+    {
+        public string BotToken { get; set; }
+        public string HostAddress { get; set; }
+        public string NgrokAddress { get; set; }
+        public string RapidApiKey { get; set; }
+        public string ImdbApiUrl { get; set; }
+		public string WeatherToken { get; set; }
+    }
+}
