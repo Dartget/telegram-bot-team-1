bf2836f5e5f0e79fb3c792e650763ce6936e619f
Egor-N-S <63055406+Egor-S-N@users.noreply.github.com>
2021-11-17 16:37:21 +0300

    Add files via upload

---
+++ telegram-bot/Weather_test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TelegramBot.Services.Weather;
+namespace Weather_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
