b5015ccb4b024e6f1cd3f88f6aa0ff902a012022
Egor-N-S <63055406+Egor-S-N@users.noreply.github.com>
2021-11-17 17:33:34 +0300

    Update UnitTest1.cs

--- telegram-bot/Weather_test/UnitTest1.cs
+++ telegram-bot/Weather_test/UnitTest1.cs
@@ -8,6 +8,26 @@ namespace Weather_test
         [TestMethod]
         public void TestMethod1()
         {
+            string[] message = new string[] { "/GetWeather", "Moscow" };
+            string city = WeatherService.OneOrMore(message);
+
+            Assert.AreEqual(message[1], city);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string[] message = new string[] { "/GetWeather", "Нижний","Новгород" };
+            string city1 = WeatherService.OneOrMore(message);
+
+            Assert.AreEqual(message[1]+" "+message[2], city1);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string[] message = new string[] { "/GetWeather", "1", "2","3" };
+            string city2 = WeatherService.OneOrMore(message);
+
+            Assert.AreEqual(message[1] + " " + message[2]+ " " + message[3], city2);
         }
     }
 }
